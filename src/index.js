84fb5b87bd5490b2ceabaefb111d850908e3e28d
sgvcool <sgvcool@gmail.com>
2019-07-22 18:40:32 +0300

    for each elements

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
 
 const App = () => {
+
+    const todoData = [
+        {label: 'Drink Coffee', important: false},
+        {label: 'Make awesome app', important: true},
+        {label: 'Have a lunch', important: false}
+    ];
+
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <TodoList />
+            <TodoList todos={todoData}/>
         </div>
     );
 };
