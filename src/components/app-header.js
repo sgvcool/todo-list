141b295c6c9f91e212e60d19c08478d98894757e
sgvcool <sgvcool@gmail.com>
2019-07-23 14:02:23 +0300

    folder per component

--- src/components/app-header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AppHeader = () => {
-    return (<h1>My todo List</h1>);
-};
-
-export default AppHeader;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
+import App from './components/app';
 
-const App = () => {
-
-    const todoData = [
-        {label: 'Drink Coffee', important: false},
-        {label: 'Make awesome app', important: true},
-        {label: 'Have a lunch', important: false}
-    ];
-
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList todos={todoData}/>
-        </div>
-    );
-};
-
-ReactDOM.render(<App />, 
-    document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
